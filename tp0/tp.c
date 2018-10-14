08598aeaceec59f881f35b6880beeabdf7edbb61
Nicolas <nicolas.dureisseix@etu.enseeiht.fr>
2018-10-14 17:32:10 +0200

    clean

--- tp0/tp.c
+++ tp0/tp.c
@@ -8,7 +8,7 @@ extern uint32_t __kernel_end__;
 
 void tp()
 {
-    multiboot_memory_map_t *start;
+  multiboot_memory_map_t *start;
 	multiboot_memory_map_t *end;
 	
 	debug("kernel mem [0x%x - 0x%x]\n", &__kernel_start__, &__kernel_end__);
@@ -17,8 +17,8 @@ void tp()
 	start = (multiboot_memory_map_t *) info -> mbi -> mmap_addr;
 	end = (multiboot_memory_map_t *) ( info -> mbi -> mmap_addr + info -> mbi -> mmap_length);
     
-    while (start < end) { 
-		debug("0x%llx - 0x%llx %d\n", start -> addr, start -> len, start -> type);
+  while (start < end) { 
+		debug("0x%llx - 0x%llx %d\n", start -> addr, start -> addr + start -> len, start -> type);
 		start++;
 	}
 }
